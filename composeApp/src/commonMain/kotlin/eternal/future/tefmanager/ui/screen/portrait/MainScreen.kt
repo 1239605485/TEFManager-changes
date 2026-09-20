6096d23f2084a49068b44aae9817de31213da18c
1239605485 <126228980+1239605485@users.noreply.github.com>
2026-09-20 15:03:15 +0800

    ui

--- composeApp/src/commonMain/kotlin/eternal/future/tefmanager/ui/screen/portrait/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/eternal/future/tefmanager/ui/screen/portrait/MainScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material.icons.rounded.Widgets
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
